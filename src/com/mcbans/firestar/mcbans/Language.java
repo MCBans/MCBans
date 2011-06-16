19d4b71dab742acc70ce2065739dadb3fd3c8392
Sean Gordon <sean_aldon@hotmail.co.uk>
2011-06-16 03:30:03 +0100

    Changed package names. Messed up last commit.

---
+++ src/com/mcbans/firestar/mcbans/Language.java
@@ -0,0 +1,29 @@
+package com.mcbans.firestar.mcbans;
+
+import java.io.File;
+
+import org.bukkit.util.config.Configuration;
+
+public class Language{
+	private Configuration config;
+	public Language( String filename ){
+		File plugin_settings = new File("plugins/mcbans/language/"+filename+".yml");
+		config = new Configuration(plugin_settings);
+		config.load();
+	}
+	public String getFormat( String Message ){
+		return config.getString( Message, "No language file loaded!" );
+	}
+	public String getFormat( String Message, String PlayerName){
+		return config.getString( Message, "No language file loaded!" ).replaceAll("%PLAYER%", PlayerName);
+	}
+	public String getFormat( String Message, String PlayerName, String PlayerAdmin ){
+		return config.getString( Message, "No language file loaded!" ).replaceAll("%PLAYER%", PlayerName).replaceAll("%ADMIN%", PlayerAdmin);
+	}
+	public String getFormat( String Message, String PlayerName, String PlayerAdmin, String Reason ){
+		return config.getString( Message, "No language file loaded!" ).replaceAll("%PLAYER%", PlayerName).replaceAll("%ADMIN%", PlayerAdmin).replaceAll("%REASON%", Reason);
+	}
+	public String getFormat( String Message, String PlayerName, String PlayerAdmin, String Reason, String PlayerIP ){
+		return config.getString( Message, "No language file loaded!" ).replaceAll("%PLAYER%", PlayerName).replaceAll("%PLAYERIP%", PlayerIP).replaceAll("%ADMIN%", PlayerAdmin).replaceAll("%REASON%", Reason);
+	}
+}
