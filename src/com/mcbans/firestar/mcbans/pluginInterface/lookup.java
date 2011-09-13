9499c498c6a400816317c5e3743fbe4b897bb9f6
Adam Walker <adamwalker1995@hotmail.com>
2011-09-13 18:57:14 +0200

    Updated the lookup, added more colour for easy identification of key points. Restructured global/local ban display.

--- src/com/mcbans/firestar/mcbans/pluginInterface/lookup.java
+++ src/com/mcbans/firestar/mcbans/pluginInterface/lookup.java
@@ -21,7 +21,7 @@ public class lookup extends Thread {
 	@Override
 	public void run(){
 		if(MCBans.getMode()){
-			MCBans.broadcastPlayer( PlayerAdmin, ChatColor.DARK_RED + "Offline Mode!" );
+			MCBans.broadcastPlayer( PlayerAdmin, "MCBans is currently in " + ChatColor.DARK_RED + "OFFLINE" + ChatColor.WHITE + " mode." );
 			return;
 		}
 		MCBans.log.write( PlayerAdmin + " has looked up the " + PlayerName + "!" );
@@ -32,12 +32,18 @@ public class lookup extends Thread {
         url_items.put("exec", "playerLookup");
         JSONObject result = webHandle.hdl_jobj(url_items);
         try {
-			MCBans.broadcastPlayer( PlayerAdmin, ChatColor.YELLOW + PlayerName + " has " + result.getString("total") + " ban(s) .:. " + result.getString("reputation") + "/10 Reputation" );
-	        for (int v = 0; v < result.getJSONArray("local").length(); v++) {
-	        	MCBans.broadcastPlayer( PlayerAdmin, "[Local] " + ChatColor.AQUA + result.getJSONArray("local").getString(v) );
+			MCBans.broadcastPlayer( PlayerAdmin, "Player " + ChatColor.AQUA + PlayerName + ChatColor.WHITE + " has " + ChatColor.DARK_RED + result.getString("total") + " ban(s) " + ChatColor.WHITE + " and " + ChatColor.LIGHT_PURPLE + result.getString("reputation") + " REP" + ChatColor.WHITE + "." );
+	        if (result.getJSONArray("global").length() > 0) {
+	        	MCBans.broadcastPlayer( PlayerAdmin, ChatColor.DARK_RED + "Global bans");
+	        	for (int v = 0; v < result.getJSONArray("global").length(); v++) {
+	        		MCBans.broadcastPlayer( PlayerAdmin, result.getJSONArray("global").getString(v) );
+	        	}
 	        }
-	        for (int v = 0; v < result.getJSONArray("global").length(); v++) {
-	        	MCBans.broadcastPlayer( PlayerAdmin, "[Global] " + ChatColor.DARK_RED + result.getJSONArray("global").getString(v) );
+	        if (result.getJSONArray("local").length() > 0) {
+	        	MCBans.broadcastPlayer( PlayerAdmin, ChatColor.YELLOW + "Local bans");
+	        	for (int v = 0; v < result.getJSONArray("local").length(); v++) {
+	        		MCBans.broadcastPlayer( PlayerAdmin, result.getJSONArray("local").getString(v) );
+	        	}
 	        }
         } catch (JSONException e) {
         } catch (NullPointerException e) {
