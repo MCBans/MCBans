a606b4b4859d9e4a7eeb8d79f0de56067acc00fd
syamn <admin@sakura-server.net>
2012-11-01 02:00:07 +0900

    Fix local ban permission node

--- src/com/mcbans/firestar/mcbans/BanType.java
+++ src/com/mcbans/firestar/mcbans/BanType.java
@@ -4,7 +4,7 @@ import com.mcbans.firestar.mcbans.permission.Perms;
 
 public enum BanType {
     GLOBAL  ("globalBan", Perms.BAN_GLOBAL),
-    LOCAL   ("localBan", Perms.BAN_GLOBAL),
+    LOCAL   ("localBan", Perms.BAN_LOCAL),
     TEMP    ("tempBan", Perms.BAN_TEMP),
 
     UNBAN   ("unBan", Perms.UNBAN),
