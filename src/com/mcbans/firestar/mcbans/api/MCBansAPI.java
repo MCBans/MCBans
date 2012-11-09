0bb04a63b6da8ec2b0911c74aac53021045dac49
syamn <admin@sakura-server.net>
2012-11-09 11:48:33 +0900

    Change MCBansAPI

--- src/com/mcbans/firestar/mcbans/api/MCBansAPI.java
+++ src/com/mcbans/firestar/mcbans/api/MCBansAPI.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import com.mcbans.firestar.mcbans.BanType;
 import com.mcbans.firestar.mcbans.BukkitInterface;
 import com.mcbans.firestar.mcbans.pluginInterface.Ban;
+import com.mcbans.firestar.mcbans.pluginInterface.Kick;
 
 public class MCBansAPI {
     private final BukkitInterface plugin;
@@ -14,7 +15,7 @@ public class MCBansAPI {
         this.plugin = plugin;
     }
 
-    private void Ban(BanType type, String targetName, String senderName, String reason, String duration, String measure){
+    private void ban(BanType type, String targetName, String senderName, String reason, String duration, String measure){
         // check null
         if (targetName == null || senderName == null){
             return;
@@ -31,24 +32,33 @@ public class MCBansAPI {
         triggerThread.start();
     }
 
-    public void LocalBan(String targetName, String senderName, String reason){
+    public void localBan(String targetName, String senderName, String reason){
         reason = (reason == null || reason == "") ? plugin.settings.getString("defaultLocal") : reason;
-        this.Ban(BanType.LOCAL, targetName, senderName, reason, "", "");
+        this.ban(BanType.LOCAL, targetName, senderName, reason, "", "");
     }
 
-    public void GlobalBan(String targetName, String senderName, String reason){
+    public void globalBan(String targetName, String senderName, String reason){
         if (reason == null || reason == "") return;
-        this.Ban(BanType.GLOBAL, targetName, senderName, reason, "", "");
+        this.ban(BanType.GLOBAL, targetName, senderName, reason, "", "");
     }
 
-    public void TempBan(String targetName, String senderName, String reason, String duration, String measure){
+    public void tempBan(String targetName, String senderName, String reason, String duration, String measure){
         reason = (reason == null || reason == "") ? plugin.settings.getString("defaultTemp") : reason;
         duration = (duration == null) ? "" : duration;
         measure = (measure == null) ? "" : measure;
-        this.Ban(BanType.TEMP, targetName, senderName, reason, duration, measure);
+        this.ban(BanType.TEMP, targetName, senderName, reason, duration, measure);
     }
 
-    public void UnBan(String targetName, String senderName){
-        this.Ban(BanType.UNBAN, targetName, senderName, "", "", "");
+    public void unBan(String targetName, String senderName){
+        this.ban(BanType.UNBAN, targetName, senderName, "", "", "");
+    }
+
+    public void kick(String targetName, String senderName, String reason){
+        reason = (reason == null || reason == "") ? plugin.settings.getString("defaultKick") : reason;
+
+        // Start
+        Kick kickPlayer = new Kick(plugin.settings, plugin, targetName, senderName, reason);
+        Thread triggerThread = new Thread(kickPlayer);
+        triggerThread.start();
     }
 }
