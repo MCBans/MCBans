ca754ae0d8a71e9d9ea9639f5a29f381df2b3264
BeastsMC <beastsmc@gmail.com>
2014-08-01 14:09:52 -1000

    Fix late night stupidity.

--- src/com/mcbans/firestar/mcbans/bukkitListeners/PlayerListener.java
+++ src/com/mcbans/firestar/mcbans/bukkitListeners/PlayerListener.java
@@ -412,7 +412,7 @@ public class PlayerListener implements Listener {
                 return;
             }
             // check alternate accounts
-            else if (config.isEnableMaxAlts() && config.getMaxAlts() < Integer.valueOf(s[3]) && Perms.EXEMPT_MAXALTS.has(event.getName())) {
+            else if (config.isEnableMaxAlts() && config.getMaxAlts() < Integer.valueOf(s[3]) && !Perms.EXEMPT_MAXALTS.has(event.getName())) {
                 event.disallow(Result.KICK_BANNED, _("overMaxAlts"));
                 return;
             }
